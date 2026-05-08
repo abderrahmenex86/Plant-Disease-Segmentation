f6ae8e42475995048617f0f82177aa13974c217e
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-05-08 10:14:19 +0100

    Add segmentation model

---
+++ model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import segmentation_models_pytorch as smp
+
+
+class DiseaseSegmenter(nn.Module):
+    def __init__(
+        self, encoder_name="mobilenet_v3_small", encoder_weights="imagenet"
+    ):
+        super().__init__()
+        self.model = smp.Unet(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            in_channels=3,
+            classes=1,
+        )
+
+    def forward(self, x):
+        return self.model(x)
