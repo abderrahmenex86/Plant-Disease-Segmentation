e8382207601ce0ca6d3b4fccf387bb3e81d18a49
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-05-14 22:26:24 +0100

    Update model to use pytorch's deeplabv3

--- models.py
+++ models.py
@@ -1,34 +1,57 @@
 import segmentation_models_pytorch as smp
 import torch.nn as nn
-from torchvision.models import MobileNet_V3_Small_Weights, mobilenet_v3_small
+from torchvision.models import MobileNet_V3_Large_Weights, mobilenet_v3_large
+from torchvision.models.segmentation import (
+    DeepLabV3_MobileNet_V3_Large_Weights,
+    deeplabv3_mobilenet_v3_large,
+)
+
+
+class DiseaseSegmenter(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = deeplabv3_mobilenet_v3_large(
+            weights=DeepLabV3_MobileNet_V3_Large_Weights.DEFAULT, weights_backbone=MobileNet_V3_Large_Weights.DEFAULT
+        )
+
+        in_channels = self.model.classifier[4].in_channels
+        self.model.classifier[4] = nn.Conv2d(in_channels, 1, kernel_size=(1, 1))
+
+        if self.model.aux_classifier:
+            in_channels_aux = self.model.aux_classifier[4].in_channels
+            self.model.aux_classifier[4] = nn.Conv2d(in_channels_aux, 1, kernel_size=(1, 1))
+
+    def forward(self, x):
+        x = self.model(x)
+        if self.training:
+            return x
+        return x["out"]
 
 
 class DiseaseClassifier(nn.Module):
     def __init__(self, num_classes=115):
         super().__init__()
-        base_model = mobilenet_v3_small(
-            weights=MobileNet_V3_Small_Weights.DEFAULT
-        )
-        self.backbone = base_model.features
-        self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.flatten = nn.Flatten()
-        self.dropout = nn.Dropout(p=0.3)
-        self.classifier_head = nn.Linear(576, num_classes)
+        self.model = mobilenet_v3_large(weights=MobileNet_V3_Large_Weights.DEFAULT)
+
+        in_features = self.model.classifier[3].in_features
+
+        self.model.classifier[2] = nn.Dropout(p=0.5, inplace=True)
+        self.model.classifier[3] = nn.Linear(in_features, num_classes)
 
     def forward(self, x):
-        x = self.dropout(self.flatten(self.pool(self.backbone(x))))
-        return self.classifier_head(x)
+        x = self.model(x)
+        return x
 
 
-class DiseaseSegmenter(nn.Module):
-    def __init__(self, encoder_name="mobilenet_v2", encoder_weights="imagenet"):
-        super().__init__()
-        self.model = smp.Unet(
-            encoder_name=encoder_name,
-            encoder_weights=encoder_weights,
-            in_channels=3,
-            classes=1,
-        )
-
-    def forward(self, x):
-        return self.model(x)
+# class DiseaseSegmenter(nn.Module):
+#     def __init__(self, encoder_name="mobilenet_v2", encoder_weights="imagenet"):
+#         super().__init__()
+#         self.model = smp.Unet(
+#             encoder_name=encoder_name,
+#             encoder_weights=encoder_weights,
+#             in_channels=3,
+#             classes=1,
+#         )
+#
+#     def forward(self, x):
+#         return self.model(x)
