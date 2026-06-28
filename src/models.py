ce9d512e84ed0f0a3e095e095509b64fc5d51e97
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-06-28 19:07:20 +0100

    rewrite models class adding: UNet, DeepLabV3, SegFormer, and LinkNet

--- src/models.py
+++ src/models.py
@@ -1,90 +1,101 @@
-import segmentation_models_pytorch as smp
 import torch
 import torch.nn as nn
-from torchvision.models import MobileNet_V3_Large_Weights
-from torchvision.models.segmentation import (
-    DeepLabV3_MobileNet_V3_Large_Weights,
-    deeplabv3_mobilenet_v3_large,
-)
+import torch.nn.functional as F
 
 
-class UNetSegmenter(nn.Module):
-    def __init__(self, num_classes=116, encoder_name="mobilenet_v2", encoder_weights="imagenet"):
+class DoubleConv(nn.Module):
+    def __init__(self, in_c, out_c):
         super().__init__()
-        self.model = smp.Unet(
-            encoder_name=encoder_name,
-            encoder_weights=encoder_weights,
-            in_channels=3,
-            classes=num_classes,
+        self.net = nn.Sequential(
+            nn.Conv2d(in_c, out_c, 3, padding=1, bias=False),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_c, out_c, 3, padding=1, bias=False),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
         )
 
     def forward(self, x):
-        return self.model(x)
+        return self.net(x)
 
 
-class DeepLabV3Segmenter(nn.Module):
-    def __init__(self, num_classes=116, pretrained=True):
+class UNet(nn.Module):
+
+    def __init__(self, in_ch=3, num_classes=116, base_c=64):
         super().__init__()
-        weights = DeepLabV3_MobileNet_V3_Large_Weights.DEFAULT if pretrained else None
-        weights_backbone = MobileNet_V3_Large_Weights.DEFAULT if pretrained else None
-
-        self.model = deeplabv3_mobilenet_v3_large(
-            weights=weights,
-            weights_backbone=weights_backbone,
-            aux_loss=True,
-        )
-
-        in_channels = self.model.classifier[4].in_channels
-        self.model.classifier[4] = nn.Conv2d(in_channels, num_classes, kernel_size=(1, 1))
-
-        if self.model.aux_classifier:
-            in_channels_aux = self.model.aux_classifier[4].in_channels
-            self.model.aux_classifier[4] = nn.Conv2d(in_channels_aux, num_classes, kernel_size=(1, 1))
+        self.inc = DoubleConv(in_ch, base_c)
+        self.d1 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(base_c, base_c * 2))
+        self.d2 = nn.Sequential(nn.MaxPool2d(2), DoubleConv(base_c * 2, base_c * 4))
+        self.u1 = nn.ConvTranspose2d(base_c * 4, base_c * 2, 2, stride=2)
+        self.c1 = DoubleConv(base_c * 4, base_c * 2)
+        self.u2 = nn.ConvTranspose2d(base_c * 2, base_c, 2, stride=2)
+        self.c2 = DoubleConv(base_c * 2, base_c)
+        self.outc = nn.Conv2d(base_c, num_classes, 1)
 
     def forward(self, x):
-        x = self.model(x)
-        if self.training:
-            return x
-        return x["out"]
+        x1, x2 = self.inc(x), self.d1(self.inc(x))
+        x = self.c1(torch.cat([self.u1(self.d2(x2)), x2], dim=1))
+        return self.outc(self.c2(torch.cat([self.u2(x), x1], dim=1)))
 
 
-class MulticlassDiceLoss(nn.Module):
-    def __init__(self, aux_weight=0.3):
+class DeepLabV3(nn.Module):
+
+    def __init__(self, num_classes):
         super().__init__()
-        self.ce = nn.CrossEntropyLoss()
-        self.dice = smp.losses.DiceLoss(mode=smp.losses.MULTICLASS_MODE, from_logits=True)
-        self.aux_weight = aux_weight
+        from torchvision.models.segmentation import (
+            DeepLabV3_MobileNet_V3_Large_Weights,
+            deeplabv3_mobilenet_v3_large,
+        )
 
-    def _compute_loss(self, pred, target):
-        return self.ce(pred, target) + self.dice(pred, target)
+        self.net = deeplabv3_mobilenet_v3_large(weights=DeepLabV3_MobileNet_V3_Large_Weights.DEFAULT)
+        self.net.classifier[4] = nn.Conv2d(256, num_classes, 1)
+        self.net.aux_classifier = None
 
-    def forward(self, outputs, masks):
-        masks = masks.squeeze(1).long()
-
-        if isinstance(outputs, dict):
-            main_loss = self._compute_loss(outputs["out"], masks)
-            aux_loss = self._compute_loss(outputs["aux"], masks)
-            return (1 - self.aux_weight) * main_loss + self.aux_weight * aux_loss
-
-        return self._compute_loss(outputs, masks)
+    def forward(self, x):
+        return self.net(x)["out"]
 
 
-class BCEDiceLoss(nn.Module):
-    def __init__(self, pos_weight=None, aux_weight=0.3):
+class LinkNet(nn.Module):
+
+    def __init__(self, num_classes):
         super().__init__()
-        self.bce = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-        self.dice = smp.losses.DiceLoss(mode=smp.losses.BINARY_MODE, from_logits=True)
-        self.aux_weight = aux_weight
+        import segmentation_models_pytorch as smp
 
-    def _compute_loss(self, pred, target):
-        return self.bce(pred, target) + self.dice(pred, target)
+        self.net = smp.Linknet(encoder_name="mobilenet_v2", encoder_weights="imagenet", classes=num_classes)
 
-    def forward(self, outputs, masks):
-        masks = masks.to(dtype=torch.float32)
+    def forward(self, x):
+        return self.net(x)
 
-        if isinstance(outputs, dict):
-            main_loss = self._compute_loss(outputs["out"], masks)
-            aux_loss = self._compute_loss(outputs["aux"], masks)
-            return (1 - self.aux_weight) * main_loss + self.aux_weight * aux_loss
 
-        return self._compute_loss(outputs, masks)
+class SegFormer(nn.Module):
+
+    def __init__(self, num_classes):
+        super().__init__()
+        import segmentation_models_pytorch as smp
+
+        self.net = smp.Unet(encoder_name="mit_b0", encoder_weights="imagenet", classes=num_classes)
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class FocalDiceLoss(nn.Module):
+    def __init__(self, num_classes, alpha=0.25, gamma=2.0):
+        super().__init__()
+        self.nc, self.alpha, self.gamma = num_classes, alpha, gamma
+
+    def forward(self, p, t):
+        t_long = t.squeeze(1).long()
+        if self.nc == 1:
+            ce = F.binary_cross_entropy_with_logits(p, t.float(), reduction="none")
+            pt = torch.exp(-ce)
+            focal = (self.alpha * (1 - pt) ** self.gamma * ce).mean()
+            p_sig = torch.sigmoid(p)
+            dice = 1 - (2.0 * (p_sig * t).sum((2, 3)) / (p_sig.sum((2, 3)) + t.sum((2, 3)) + 1e-6)).mean()
+        else:
+            ce = F.cross_entropy(p, t_long, reduction="none")
+            pt = torch.exp(-ce)
+            focal = ((1 - pt) ** self.gamma * ce).mean()
+            p_s, t_oh = F.softmax(p, 1), F.one_hot(t_long, self.nc).permute(0, 3, 1, 2).float()
+            dice = 1 - (2.0 * (p_s * t_oh).sum((2, 3)) / (p_s.sum((2, 3)) + t_oh.sum((2, 3)) + 1e-6)).mean()
+        return focal + dice
